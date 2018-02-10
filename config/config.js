3f1b11736cc4dac6b6cea98a8832416a1658efaa
Abhishek Singh <abhishek.codeyo@gmail.com>
2018-02-10 12:51:57 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ var CONFIG = CONFIG || {};
 CONFIG.prompt = function(cwd, user) {
    if (user)
       return '<span class="user">' + user +
-          '</span>@<span class="host">https://theabhisheksingh.github.io/</span>:<span class="cwd">' +
+          '</span>@<span class="host">https://theabhisheksingh.github.io</span>:<span class="cwd">' +
           cwd + '</span>$ ';
    return 'jsterm1.0 $ ';
 };
